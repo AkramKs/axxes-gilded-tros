f23820d8cc9affc6a8a837cc68982c8b06730670
Akram <Akram-kasimi@outlook.be>
2025-04-19 00:58:39 +0200

    Add GoodWineItem and LegendaryItem logic tests in GildedTrosTest

--- Java/src/test/java/com/gildedtros/GildedTrosTest.java
+++ Java/src/test/java/com/gildedtros/GildedTrosTest.java
@@ -88,4 +88,44 @@ public class GildedTrosTest {
         }
     }
 
+    @Nested
+    class BackstagePass {
+
+        @Test
+        void increasesBy1WhenMoreThan10DaysLeft() {
+            items = new Item[]{new Item("Backstage passes for Re:Factor", 11, 20)};
+            app = new GildedTros(items);
+            app.updateQuality();
+            assertEquals(10, items[0].sellIn);
+            assertEquals(21, items[0].quality);
+        }
+
+        @Test
+        void increasesBy2When10OrLessDaysLeft() {
+            items = new Item[]{new Item("Backstage passes for Re:Factor", 10, 20)};
+            app = new GildedTros(items);
+            app.updateQuality();
+            assertEquals(9, items[0].sellIn);
+            assertEquals(22, items[0].quality);
+        }
+
+        @Test
+        void increasesBy3When5OrLessDaysLeft() {
+            items = new Item[]{new Item("Backstage passes for HAXX", 5, 20)};
+            app = new GildedTros(items);
+            app.updateQuality();
+            assertEquals(4, items[0].sellIn);
+            assertEquals(23, items[0].quality);
+        }
+
+        @Test
+        void dropsToZeroAfterConcert() {
+            items = new Item[]{new Item("Backstage passes for HAXX", 0, 20)};
+            app = new GildedTros(items);
+            app.updateQuality();
+            assertEquals(-1, items[0].sellIn);
+            assertEquals(0, items[0].quality);
+        }
+    }
+
 }
