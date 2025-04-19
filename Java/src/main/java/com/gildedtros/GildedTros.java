b922124f4555442f91331ebbbe3d806aed69a8cd
Akram <Akram-kasimi@outlook.be>
2025-04-19 20:27:20 +0200

    Split updateQuality into multi methods

--- Java/src/main/java/com/gildedtros/GildedTros.java
+++ Java/src/main/java/com/gildedtros/GildedTros.java
@@ -3,66 +3,79 @@ package com.gildedtros;
 import static com.gildedtros.constants.ItemNames.*;
 
 class GildedTros {
-    Item[] items;
+    private final Item[] items;
 
     public GildedTros(Item[] items) {
         this.items = items;
     }
 
-    //Todo Split for loops into smaller methodes
-
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals(GOOD_WINE)
-                    && !items[i].name.equals(BACKSTAGE_PASSES_REFACTOR)
-                    && !items[i].name.equals(BACKSTAGE_PASSES_HAXX))
-            {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+        for (Item item : items) {
+            updateItemQuality(item);
+            updateSellIn(item);
+            handleExpiredItem(item);
+        }
+    }
+
+    private void updateItemQuality(Item item) {
+        if (isSpecialItem(item)) {
+            increaseQuality(item);
+
+            if (isBackstagePass(item)) {
+                if (item.sellIn < 11) {
+                    increaseQuality(item);
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals(BACKSTAGE_PASSES_REFACTOR) || items[i].name.equals(BACKSTAGE_PASSES_HAXX)) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
+                if (item.sellIn < 6) {
+                    increaseQuality(item);
                 }
             }
-
-            if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals(GOOD_WINE)) {
-                    if (!items[i].name.equals(BACKSTAGE_PASSES_REFACTOR) && !items[i].name.equals(BACKSTAGE_PASSES_HAXX)) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals(B_DAWG_KEYCHAIN)) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = 0;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        } else {
+            if (item.quality > 0 && !isLegendary(item)) {
+                decreaseQuality(item);
             }
         }
+    }
+
+    private void updateSellIn(Item item) {
+        if (!isLegendary(item)) {
+            item.sellIn--;
+        }
+    }
+
+    private void handleExpiredItem(Item item) {
+        if (item.sellIn >= 0) return;
+
+        if (item.name.equals(GOOD_WINE)) {
+            increaseQuality(item);
+        } else if (isBackstagePass(item)) {
+            item.quality = 0;
+        } else {
+            if (item.quality > 0 && !isLegendary(item)) {
+                decreaseQuality(item);
+            }
+        }
+    }
+
+    private boolean isSpecialItem(Item item) {
+        return item.name.equals(GOOD_WINE) || isBackstagePass(item);
+    }
+
+    private boolean isBackstagePass(Item item) {
+        return item.name.equals(BACKSTAGE_PASSES_REFACTOR)
+                || item.name.equals(BACKSTAGE_PASSES_HAXX);
+    }
+
+    private boolean isLegendary(Item item) {
+        return item.name.equals(B_DAWG_KEYCHAIN);
+    }
+
+    private void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    private void decreaseQuality(Item item) {
+        item.quality--;
     }
 }
